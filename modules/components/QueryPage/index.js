491dbaa712b0420103cbfcbcc6ddb219fe396fc5
Vladimir Danchenkov <vladimir.danchenkov@gmail.com>
2016-04-29 16:19:14 +0200

    extract query page view

--- modules/components/QueryPage/index.js
+++ modules/components/QueryPage/index.js
@@ -1,29 +1,50 @@
 import React from 'react'
-import { args, result, header, queryContainer } from './styles.css'
+import styles from './styles.css'
 import Codemirror from 'react-codemirror'
 
 export default React.createClass({
-  getInitialState: () => ({
-    arguments: '[1, 2, 3]',
-    result: '3'
-  }),
-  updateCode: newCode => {
-    this.setState({ code: newCode })
+  componentDidMount() {
+    this.updateResults()
   },
-  onArgumentsChange: () => {
-
+  getInitialState: () => ({
+    args: '[1, 2, 3]',
+    result: '3',
+    suggestions: [1,2,3]
+  }),
+  updateResults() {
+    this.setState({suggestions: [`f(${this.state.args}) == ${this.state.result}`]})
+  },
+  onResultChange: function (result) {
+    this.setState({ result })
+    this.updateResults()
+  },
+  onArgumentsChange: function (args) {
+    this.setState({ args })
+    this.updateResults()
   },
   render() {
-  const styles = {};
     return (
-      <div>
-        <h2 className={header}>Function arguments</h2>
-        <div className={queryContainer}>
-          <Codemirror className={args} value={this.state.arguments} onChange={this.onArgumentsChange}/>
-          <Codemirror className={result} value={this.state.result}/>
-        </div>
-      </div>
+        <QueryPageView
+            args={this.state.args}
+            result={this.state.result}
+            onResultChange={this.onResultChange}
+            onArgumentsChange={this.onArgumentsChange}
+            suggestions={this.state.suggestions}
+        />
     )
   }
 })
 
+export const QueryPageView = ({args, result, onResultChange, onArgumentsChange, suggestions}) => {
+  return (
+      <div>
+        <div className={styles.queryContainer}>
+          <Codemirror className={styles.args} value={args} onChange={onArgumentsChange}/>
+          <Codemirror className={styles.result} value={result} onChange={onResultChange}/>
+        </div>
+        {
+          suggestions.map(suggestion => <div>{suggestion}</div>)
+        }
+      </div>
+  );
+}
