9ec1b4ab3340686592defc5948d4d14c502e6ea5
Vladimir Danchenkov <vladimir.danchenkov@gmail.com>
2016-06-19 16:03:33 +0400

    Add links to documentation

--- modules/components/Suggestions/index.js
+++ modules/components/Suggestions/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import styles from './styles.css'
 import SyntaxHighlight from './../SyntaxHighlight'
+import DocLink from './DocLink'
 
 export default ({suggestions, loading}) => (
   <div className={styles.component}>
     {
       suggestions.map(({ library, name, args, argsLabels }) => (
           <div key={library + name + argsLabels}>
-            <SyntaxHighlight code={library + '.' + name +'(' + argsLabels.join(', ') + ')'}/>
+            {library}
+            <span className="token punctuation">.</span>
+            <span className="token function"><DocLink module={library} func={name}>{name}</DocLink></span>
+            <SyntaxHighlight code={'(' + argsLabels.join(', ') + ')'}/>
           </div>
       ))
     }
