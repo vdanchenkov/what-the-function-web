1d025ad6880d6885caf853811412fba0cbb87cbb
Vladimir Danchenkov <vladimir.danchenkov@gmail.com>
2016-09-06 01:55:11 +0400

    add google analytics

--- modules/client.js
+++ modules/client.js
@@ -27,3 +27,11 @@ if (module.hot) {
     )
   })
 }
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga')
+
+ga('create', 'UA-83722336-1', 'auto')
+ga('send', 'pageview')
