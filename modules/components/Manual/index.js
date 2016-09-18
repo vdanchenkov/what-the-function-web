e132ae9607235603206c4860830b911e50178202
Vladimir Danchenkov <vladimir.danchenkov@gmail.com>
2016-09-18 20:32:44 +0400

    change wording and style of Manual

--- modules/components/Manual/index.js
+++ modules/components/Manual/index.js
@@ -9,18 +9,18 @@ const Manual = () => (
     </div>
     <div>
       <p>
-        This app aids in search for library functions by arguments and expected result.  
+        This app searches for functions and snippets in selected modules by argument list and expected result.  
       </p>
       <p>
-        Search is done by brute force. It tries all combinations of functions and arguments
-        and shows matches. First 3 arguments will be permuted. So search 
-        for <code>wtf(2, 4) == 2</code> will match <code>4 - 2</code>. Other arguments are
-        kept as is for performance reasons. In case single test takes more then {
-        timeout } ms, it will be skipped.
+        It tries permutations of the first three arguments so <code>wtf(2, 4).isEqual(2)</code> 
+        will match <code>4 - 2</code> and <code>4 / 2</code>. Other arguments are kept as is for performance reasons.
+      </p>
+      <p> 
+        In case single check takes more then { timeout } ms, it will be skipped.
       </p>
       <p>
-        Modules are loaded via <a href="https://unpkg.com" target="_blank">unpkg.com</a>. 
-        Thanks to this project your can add any npm module to the search.
+        Modules are loaded from <a href="https://unpkg.com" target="_blank">unpkg.com</a>. 
+        Thanks to it you will be able to add arbitrary modules in the next version.
       </p>
     </div>
   </div>
@@ -30,14 +30,27 @@ export default Manual
 
 const component = [
   { fontFamily: `"Helvetica Neue", Helvetica, Arial, sans-serif` },
-  select(' p', { margin: '1rem 0', label: 'test' }),
-  select(' code', { fontStyle: 'italic', fontSize: 'larger' }),
+  select(' p', { margin: '0.75rem 0' }),
+  select(' code', {
+    fontFamily: `Consolas, "Liberation Mono", Menlo, Courier, monospace`,
+    backgroundColor: 'rgba(0,0,0,0.04)', 
+    fontSize: '85%',
+    paddingTop: '0.2em',
+    paddingBottom: '0.2em'
+  }),
+  select(' code::before', {
+    letterSpacing: '-0.2em',
+    content: '"\\00a0"'
+  }),
+  select(' code::after', {
+    letterSpacing: '-0.2em',
+    content: '"\\00a0"'
+  }),
   select(' a', { textDecoration: 'none' })
 ]
 
 const header = {
   fontWeight: 'bold',
-  fontSize: '1.2rem',
-  marginBottom: '0.2rem',
+  fontSize: '1.5rem',
   textAlign: 'center'
 }
